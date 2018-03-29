b00d7fc5532b0e6d6eedc7242cec4537c703d16d
N1kko777 <3734489@gmail.com>
2018-03-29 17:36:49 +0500

    lesson-4

--- courseJS/lesson-4/extra/script.js
+++ courseJS/lesson-4/extra/script.js
@@ -5,7 +5,8 @@ function count() {
 	let sum = 0;
 	for (let i = 0; i < col; i++) {
 		arr.push([]);
-		for(let j = 0; j < 5; j++) {
+		let rnd = Math.floor(Math.random() * 5) + 1;
+		for(let j = 0; j < rnd; j++) {
 			arr[i][j] = Math.floor(Math.random() * 11);
 			sum = sum + arr[i][j];
 		} 
